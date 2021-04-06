202c9f57801582a278cd6d03c7ac876f5446c9fb
Tom Hinsley <tomhinsley@me.com>
2021-04-06 10:55:08 +0100

    buttons no longer work after one clicked, added refresh button, buttons usable after siren sounds, added display changes on button push

--- clock.js
+++ clock.js
@@ -6,29 +6,61 @@ let doomsdayClockOneMax = 300;
 let doomsdayClockTwoMin = 120;
 let doomsdayClockTwoMax = 240;
 
-let doomsdayClockThreeMin = 5;
-let doomsdayClockThreeMax = 5;
+let doomsdayClockThreeMin = 30;
+let doomsdayClockThreeMax = 30;
 
-function doomsdayClockButtonClicked() {
-    document.getElementById('doomsdayClockActivityStatus').innerHTML = 'Countdown Started';
-}
+let timer;
+
+document.querySelector('#doomsdayClockButtonOne').addEventListener('click', e => {
+    e.target.disabled = true;
+    document.getElementById('doomsdayClockButtonOneDisplay').innerHTML = 'Countdown Initiated';
+    doomsdayClock(doomsdayClockOneMin, doomsdayClockOneMax)
+    document.querySelector('#doomsdayClockButtonTwo').disabled = true;
+    document.querySelector('#doomsdayClockButtonThree').disabled = true;
+})
+
+document.querySelector('#doomsdayClockButtonTwo').addEventListener('click', e => {
+    e.target.disabled = true;
+    doomsdayClock(doomsdayClockTwoMin, doomsdayClockTwoMax)
+    document.getElementById('doomsdayClockButtonTwoDisplay').innerHTML = 'Countdown Initiated';
+    document.querySelector('#doomsdayClockButtonOne').disabled = true;
+    document.querySelector('#doomsdayClockButtonThree').disabled = true;
+})
+
+document.querySelector('#doomsdayClockButtonThree').addEventListener('click', e => {
+    e.target.disabled = true;
+    doomsdayClock(doomsdayClockThreeMin, doomsdayClockThreeMax)
+    document.getElementById('doomsdayClockButtonThreeDisplay').innerHTML = 'Countdown Initiated';
+    document.querySelector('#doomsdayClockButtonOne').disabled = true;
+    document.querySelector('#doomsdayClockButtonTwo').disabled = true;
+})
+
+document.querySelector('#doomsdayClockResetButton').addEventListener('click', e => {
+    location.reload();
+})
 
 function doomsdayClock(min, max) {
-
-    doomsdayClockButtonClicked();
 
     let doomsdayClockSeconds =  Math.floor(Math.random() * (max - min + 1) + min);
 
     console.log('Doomsday is in ' + doomsdayClockSeconds + ' seconds');
 
-    setTimeout(doomsdayClockActive, doomsdayClockSeconds * 1000);
+    timer = setTimeout(doomsdayClockActive, doomsdayClockSeconds * 1000);
 }
 
 function doomsdayClockActive() {
     siren.play();
-    document.getElementById('doomsdayClockActivityStatus').innerHTML = 'Doomsday Clock'
+    document.querySelector('#doomsdayClockButtonOne').disabled = false;
+    document.querySelector('#doomsdayClockButtonTwo').disabled = false;
+    document.querySelector('#doomsdayClockButtonThree').disabled = false;
+    document.getElementById('doomsdayClockButtonOneDisplay').innerHTML = '3 - 4 Players Remaining';
+    document.getElementById('doomsdayClockButtonTwoDisplay').innerHTML = '2 Players Remaining';
+    document.getElementById('doomsdayClockButtonThreeDisplay').innerHTML = 'Quick Fire';
 }
 
-// add reset button (or stop button) (browser refresh)
 // Pressing button again overrides
 // add rivets around the clock
+// cancelTimeout - pass in timer variable
+
+// MAKE EACH DISPLAY CHANGE TO SHOW ACTIVE
+// MAKE NICE
