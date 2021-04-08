eb6eb84a2f09956ef479bfff08bab49dcc963b5d
Tom Hinsley <tomhinsley@me.com>
2021-04-08 15:50:45 +0100

    made app more functional/browser tested. removed button click sound as it feels faster without. reduced clock timers for testing

--- clock.js
+++ clock.js
@@ -1,55 +1,15 @@
 let siren = new Audio('siren.mp3');
-let buttonClick = new Audio('button.mp3');
 
-let doomsdayClockOneMin = 180;
-let doomsdayClockOneMax = 300;
+let doomsdayClockOneMin = 3;
+let doomsdayClockOneMax = 3;
 
-let doomsdayClockTwoMin = 120;
-let doomsdayClockTwoMax = 240;
+let doomsdayClockTwoMin = 3;
+let doomsdayClockTwoMax = 3;
 
-let doomsdayClockThreeMin = 30;
-let doomsdayClockThreeMax = 30;
+let doomsdayClockThreeMin = 3;
+let doomsdayClockThreeMax = 3;
 
 let timer;
-
-function disableButton(button) {
-    document.querySelector(button).disabled = true;
-}
-
-document.querySelector('#doomsdayClockButtonOne').addEventListener('click', e => {
-    doomsdayClock(doomsdayClockOneMin, doomsdayClockOneMax)
-    e.target.disabled = true;
-    document.getElementById('doomsdayClockButtonOneDisplay').innerText = 'Countdown Initiated';
-    disableButton('#doomsdayClockButtonTwo');
-    disableButton('#doomsdayClockButtonThree');
-    document.querySelector('#doomsdayClockButtonTwo').style.background = '#A30000';
-    document.querySelector('#doomsdayClockButtonThree').style.background = '#CC0605';
-})
-
-document.querySelector('#doomsdayClockButtonTwo').addEventListener('click', e => {
-    doomsdayClock(doomsdayClockTwoMin, doomsdayClockTwoMax)
-    e.target.disabled = true;
-    document.getElementById('doomsdayClockButtonTwoDisplay').innerText = 'Countdown Initiated';
-    disableButton('#doomsdayClockButtonOne');
-    disableButton('#doomsdayClockButtonThree');
-    document.querySelector('#doomsdayClockButtonOne').style.background = '#A30000';
-    document.querySelector('#doomsdayClockButtonThree').style.background = '#A30000';
-})
-
-document.querySelector('#doomsdayClockButtonThree').addEventListener('click', e => {
-    doomsdayClock(doomsdayClockThreeMin, doomsdayClockThreeMax)
-    e.target.disabled = true;
-    document.getElementById('doomsdayClockButtonThreeDisplay').innerText = 'Countdown Initiated';
-    disableButton('#doomsdayClockButtonOne');
-    disableButton('#doomsdayClockButtonTwo');
-    document.querySelector('#doomsdayClockButtonOne').style.background = '#A30000';
-    document.querySelector('#doomsdayClockButtonTwo').style.background = '#A30000';
-})
-
-document.querySelector('#doomsdayClockResetButton').addEventListener('click', e => {
-    buttonClick.play();
-    location.reload();
-})
 
 function doomsdayClock(min, max) {
 
@@ -60,20 +20,75 @@ function doomsdayClock(min, max) {
     timer = setTimeout(doomsdayClockActive, doomsdayClockSeconds * 1000);
 }
 
-function doomsdayClockActive() {
-    
-    siren.play();
-
-    // Enable buttons
-    document.querySelector('#doomsdayClockButtonOne').disabled = false;
-    document.querySelector('#doomsdayClockButtonTwo').disabled = false;
-    document.querySelector('#doomsdayClockButtonThree').disabled = false;
-
-    // Reset button display text
-    document.getElementById('doomsdayClockButtonOneDisplay').innerHTML = '3 - 4 Players Remaining';
-    document.getElementById('doomsdayClockButtonTwoDisplay').innerHTML = '2 Players Remaining';
-    document.getElementById('doomsdayClockButtonThreeDisplay').innerHTML = 'Quick Fire';
-
-    // Reset button "light" colour
-    document.querySelectorAll('.clockStartBackground').background = '#CC0605';
+function disableButton(button) {
+    document.querySelector(button).disabled = true;
 }
+
+function initiateCountdown(display) {
+    document.getElementById(display).innerText = 'Countdown Initiated';
+
+}
+
+function darkenButtonColour(button) {
+    document.querySelector(button).style.background = '#A30000';
+}
+
+function resetButtonColour(button) {
+    document.querySelector(button).style.background = '#FF0000';
+}
+
+function resetDisplayText() {
+    document.getElementById('doomsdayClockButtonOneDisplay').innerText = '3 - 4 Players Remaining';
+    document.getElementById('doomsdayClockButtonTwoDisplay').innerText = '2 Players Remaining';
+    document.getElementById('doomsdayClockButtonThreeDisplay').innerText = 'Quick Fire';
+}
+
+function enableButton(button) {
+    document.querySelector(button).disabled = false;
+}
+
+function doomsdayClockActive() {
+    siren.play();
+    enableButton('#doomsdayClockButtonOne');
+    enableButton('#doomsdayClockButtonTwo');
+    enableButton('#doomsdayClockButtonThree');
+    resetDisplayText();
+    resetButtonColour('#doomsdayClockButtonOne');
+    resetButtonColour('#doomsdayClockButtonTwo');
+    resetButtonColour('#doomsdayClockButtonThree');
+}
+
+
+document.querySelector('#doomsdayClockButtonOne').addEventListener('click', e => {
+    doomsdayClock(doomsdayClockOneMin, doomsdayClockOneMax)
+    disableButton('#doomsdayClockButtonOne');
+    disableButton('#doomsdayClockButtonTwo');
+    disableButton('#doomsdayClockButtonThree');
+    initiateCountdown('doomsdayClockButtonOneDisplay');
+    darkenButtonColour('#doomsdayClockButtonTwo');
+    darkenButtonColour('#doomsdayClockButtonThree');
+})
+
+document.querySelector('#doomsdayClockButtonTwo').addEventListener('click', e => {
+    doomsdayClock(doomsdayClockTwoMin, doomsdayClockTwoMax)
+    disableButton('#doomsdayClockButtonOne');
+    disableButton('#doomsdayClockButtonTwo');
+    disableButton('#doomsdayClockButtonThree');
+    initiateCountdown('doomsdayClockButtonTwoDisplay');
+    darkenButtonColour('#doomsdayClockButtonOne');
+    darkenButtonColour('#doomsdayClockButtonThree');
+})
+
+document.querySelector('#doomsdayClockButtonThree').addEventListener('click', e => {
+    doomsdayClock(doomsdayClockThreeMin, doomsdayClockThreeMax)
+    disableButton('#doomsdayClockButtonOne');
+    disableButton('#doomsdayClockButtonTwo');
+    disableButton('#doomsdayClockButtonThree');
+    initiateCountdown('doomsdayClockButtonThreeDisplay');
+    darkenButtonColour('#doomsdayClockButtonOne');
+    darkenButtonColour('#doomsdayClockButtonTwo');
+})
+
+document.querySelector('#doomsdayClockResetButton').addEventListener('click', e => {
+    location.reload();
+})
